798567dc1c1e5b02133a3981ecfdd20894f870db
oniic <onisic95@gmail.com>
2024-07-21 02:02:58 +0900

    요청시 로그 구현

---
+++ src/main/java/com/example/interceptor/web/interceptor/LogInterceptor.java
@@ -0,0 +1,38 @@
+package com.example.interceptor.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+
+    private static final String LOG_ID = "LOG_ID";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        String requestURI = request.getRequestURI();
+        String log_uuid = UUID.randomUUID().toString();
+        request.setAttribute(LOG_ID, log_uuid);
+
+        log.info("REQUEST [{}] [{}] [{}]", log_uuid, requestURI, handler);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("postHandle [{}]", modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String requestURI = request.getRequestURI();
+        String log_uuid = (String)request.getAttribute(LOG_ID);
+        log.info("RESPONSE [{}][{}]", log_uuid, requestURI);
+    }
+}
